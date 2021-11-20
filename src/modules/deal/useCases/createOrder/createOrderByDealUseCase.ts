7c750aaef3f35eb08f2782bc55e8d36781e4e882
Kaio <kaio_smoreira@hotmail.com>
2021-11-20 19:53:15 -0300

    add id deal

--- src/modules/deal/useCases/createOrder/createOrderByDealUseCase.ts
+++ src/modules/deal/useCases/createOrder/createOrderByDealUseCase.ts
@@ -2,25 +2,28 @@ import axios from "axios";
 
 import { IDealDTO } from "../../dtos/IDealDTO";
 
-async function getDeal(): Promise<IDealDTO> {
+async function getDealInfo(): Promise<IDealDTO> {
   const response = await axios.get(
     `https://api.pipedrive.com/v1/deals/?api_token=cb56f8ed2d027b2f539e93112e73ac474b00ea77`
   );
-  const data = response.data.data as IDealDTO;
+  const data = response.data.data[2] as IDealDTO;
 
   return data;
 }
+
+async function getDealItem(): Promise<IDealItem>;
 
 class CreateOrderByDealUseCase {
   //   constructor() {
   //   }
 
   async execute(): Promise<void> {
-    const deal = await getDeal();
+    const deal = await getDealInfo();
 
     console.log(deal);
 
     const {
+      id,
       title,
       value,
       status,
@@ -31,6 +34,7 @@ class CreateOrderByDealUseCase {
     } = deal as IDealDTO;
 
     const dealWon = {
+      id,
       title,
       value,
       status,
