ab696aa4c569e461075a958a38569179cac09604
Kaio <kaio_smoreira@hotmail.com>
2021-11-20 17:50:17 -0300

    create interface DTO

---
+++ src/modules/deal/dtos/IDealDTO.ts
@@ -0,0 +1,11 @@
+interface IDealDTO {
+  title: string;
+  value: number;
+  status: string;
+  products_count: string;
+  org_name: string;
+  person_name: string;
+  next_activity_subject: string;
+}
+
+export { IDealDTO };
