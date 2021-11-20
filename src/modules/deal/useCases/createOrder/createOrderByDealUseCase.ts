e7b13d74e48b20b29bb9148faeae97e146a2e22f
Kaio <kaio_smoreira@hotmail.com>
2021-11-20 17:53:29 -0300

    consume api pipedrive e add tipagem nos atributos

--- src/modules/deal/useCases/createOrder/createOrderByDealUseCase.ts
+++ src/modules/deal/useCases/createOrder/createOrderByDealUseCase.ts
@@ -1,15 +1,44 @@
 import axios from "axios";
 
+import { IDealDTO } from "../../dtos/IDealDTO";
+
+async function getDeal(): Promise<IDealDTO> {
+  const response = await axios.get(
+    `https://api.pipedrive.com/v1/deals/?api_token=cb56f8ed2d027b2f539e93112e73ac474b00ea77`
+  );
+  const data = response.data.data[0] as IDealDTO;
+
+  return data;
+}
+
 class CreateOrderByDealUseCase {
   //   constructor() {
   //   }
 
-  async execute(): Promise<any> {
-    const dealPipeDrive = await axios.get(
-      `${process.env.PIPEDRIVE_URL}/${process.env.PIPEDRIVE_API_KEY}`
-    );
+  async execute(): Promise<void> {
+    const deal = await getDeal();
 
-    return dealPipeDrive;
+    const {
+      title,
+      value,
+      status,
+      products_count,
+      org_name,
+      person_name,
+      next_activity_subject,
+    } = deal as IDealDTO;
+
+    const dealWon = {
+      title,
+      value,
+      status,
+      products_count,
+      org_name,
+      person_name,
+      next_activity_subject,
+    };
+
+    console.log(dealWon);
   }
 }
 
